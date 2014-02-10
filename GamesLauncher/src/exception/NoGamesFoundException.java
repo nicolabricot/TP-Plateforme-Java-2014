73d16054735448f848e8b3aaa53fe9ac843f4e21
nicolabricot <github@devenet.info>
2014-02-10 01:22:04 +0100

    Create a Swing Launcher

---
+++ GamesLauncher/src/exception/NoGamesFoundException.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Nicolas Devenet <nicolas@devenet.info>
+ */
+public class NoGamesFoundException extends Exception {
+}
